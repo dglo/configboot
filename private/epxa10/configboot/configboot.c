db55a1d660ae2c3fdf03d7f24d7bd098e85ffa28
Arthur Jones <arthur@lbl.gov>
2004-06-16 05:12:34 -0500

    configboot boots again, flash programming not working yet...

--- private/epxa10/configboot/configboot.c
+++ private/epxa10/configboot/configboot.c
@@ -80,7 +80,6 @@ static void prtCmds(void) {
    putst("  p [schip echip] : program flash starting at schip (0 or 1) to"
 	  " echip (0 or 1)\r\n");
    putst("  ?               : show commands\r\n");
-   putst("  hi!\r\n");
 }
 
 static inline void setBootFlash(void) { halSetFlashBoot(); }
@@ -151,7 +150,7 @@ int main(int argc, char *argv[]) {
       ST_SCHIP,
       ST_ECHIP,
    } state = ST_READY;
-   
+
    /* initialize comm... */
    initComm();
 
@@ -184,10 +183,6 @@ int main(int argc, char *argv[]) {
 	    prtSwapFlash();
 	    prtNumErrors();
 
-            putst("    numdorerrors: ");
-            puti(ndorerrors);
-            putst("\r\n");
-
 	    putst("# ");
 	 }
 	 else if (c=='f') {
@@ -218,7 +213,6 @@ int main(int argc, char *argv[]) {
 	    state = ST_SCHIP;
 	 }
 	 else if (c=='r') {
-	    putst("rebooting...\r\n\r\n");
             /* FIXME: wait for msg to get through... */
 	    reboot();
 	    /* doesn't return ... */
@@ -276,181 +270,6 @@ static int lock_chip(int chip) {
    if (chip<0 || chip>1) return 1;
    return flash_unlock(flash_chip_addr(chip), 0x00400000);
 }
-
-#if 0
-/* block size in 16 bit words 
- */
-static int blkSize(int blk) { return (blk<8) ? 4*1024 : 32*1024; }
-
-/* convert address to chip...
- */
-static int addrtochip(const void *a) {
-   const short *addr = (const short *) a;
-   const short *fs0 = (const short *) 0x41000000;
-   const short *fe0 = (const short *) (0x41400000-2);
-   const short *fs1 = (const short *) 0x41400000;
-   const short *fe1 = (const short *) (0x41800000-2);
-
-   if (addr>=fs0 && addr<=fe0)      { return 0; }
-   else if (addr>=fs1 && addr<=fe1) { return 1; }
-   return -1;
-}
-
-/* convert address to block location...
- */
-static int addrtoblock(const void *a) {
-   const short *addr = (const short *) a;
-   const short *fs0 = (const short *) 0x41000000;
-   const short *fs1 = (const short *) 0x41400000;
-   const int chip = addrtochip(a);
-   int idx, bblk;
-   if (chip==0)      { idx = addr - fs0; }
-   else if (chip==1) { idx = addr - fs1; }
-   else { return -1; }
-   bblk = idx/(32*1024);
-   return (bblk==0) ? (idx/(4*1024)) : (bblk + 7);
-}
-
-/* turn chip/blk -> addr
- */
-static void *blktoaddr(int chip, int blk) {
-   short *fs0 = (short *) 0x41000000;
-   short *fs1 = (short *) 0x41400000;
-   short *addr;
-   
-   if (chip==0)      { addr = fs0; }
-   else if (chip==1) { addr = fs1; }
-   else { return NULL; }
-
-   return addr + ( (blk<8) ? (blk*4*1024) : ( (blk-7)*32*1024) );
-}
-
-static void *flash_chip_addr(int chip) {
-   return (void *) ((chip==0) ? 0x41000000 : 0x41400000);
-}
-
-static int unlock_chip(int chip) {
-   const int sblk = addrtoblock(flash_chip_addr(chip));
-   const int eblk = addrtoblock((char *)flash_chip_addr(chip) + 0x00400000);
-   volatile short *flash;
-   int i;
-   
-   flash = blktoaddr(chip, 0);
-
-   putst("flash: ");
-   puti((int) flash);
-   putst("\r\n");
-
-   *flash = 0x50; /* clear status register... */
-   
-   for (i=sblk; i<=eblk; i++) {
-      volatile short *block = blktoaddr(chip, i);
-
-      putst("block: ");
-      puti((int) block);
-      putst("(");
-      puti(sblk);
-      putst(")\r\n");
-
-      *flash = 0x60;
-      *block = 0xd0;
-   }
-
-   /* now confirm lock...
-    */
-   *flash = 0x90;
-   for (i=sblk; i<=eblk; i++) {
-      volatile short *block = blktoaddr(chip, i);
-      const unsigned short v = *(block+2);
-      if ( (v&1) != 0 ) break; 
-   }
-   
-   /* back to read array mode... */
-   *flash = 0xff;
-
-   putst("done unlock!\r\n");
-   
-
-   /* send back confirmation...
-    */
-   return (i<=eblk) ? 1 : 0;
-}
-
-static int lock_chip(int chip) {
-   const int sblk = 0;
-   const int eblk = addrtoblock((char *)flash_chip_addr(chip) + 0x00400000);
-   volatile short *flash;
-   int i;
-   
-   flash = (volatile short *) blktoaddr(chip, 0);
-   *flash = 0x50; /* clear status register... */
-   
-   for (i=sblk; i<=eblk; i++) {
-      volatile short *block = blktoaddr(chip, i);
-      *flash = 0x60;
-      *block = 0x01;
-   }
-
-   /* now confirm lock...
-    */
-   *flash = 0x90;
-   for (i=sblk; i<=eblk; i++) {
-      volatile short *block = blktoaddr(chip, i);
-      const unsigned short v = *(block+2);
-      if ((v&1) != 1) {
-	 break;
-      }
-   }
-   
-   /* back to read array mode... */
-   *flash = 0xff;
-
-   /* send back confirmation...
-    */
-   return (i<=eblk) ? 1 : 0;
-}
-
-static int feq(const short *p1, const short *p2, int n) {
-	int i;
-	for (i=0; i<(n+1)/2; i++) if (p1[i]!=p2[i]) return 1;
-	return 0;
-}
-
-static int flash_write(void *to, const void *mem, int cnt) {
-   volatile short *addr = (volatile short *) to;
-   const short *ptr = (const short *) mem;
-   const int sblk = addrtoblock(to);
-   const int eblk = addrtoblock((const char *)to+cnt-2);
-   const int chip = addrtochip(to);
-   volatile short *flash;
-   int i;
-   
-   if (chip<0 || sblk<0 || eblk<0 || eblk<sblk) return 1;
-
-   if (chip!=addrtochip((const char *)to + cnt - 2)) return 1;
-
-   flash = blktoaddr(chip, 0);
-   *flash = 0x50; /* clear status register... */
-
-   for (i=0; i<(cnt+1)/2; i++) {
-      unsigned short v;
-      *flash = 0x40;
-      addr[i] = ptr[i];
-      while (1) {
-	 v = *(volatile short *)flash;
-	 if (v&0x80) break;
-      }
-   }
-
-   /* back to read array mode... 
-    */
-   *flash = 0xff;
-
-   /* send back confirmation...
-    */
-   return (feq(to, mem, cnt)==0) ? 0 : 1;
-}
-#endif
 
 static void programFlash(int schip, int echip) {
    void *fs; /* , *fe; */
